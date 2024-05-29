40d51828503d59bc1ed792b378f7ff02eda6905b
Sophie Tigerholm <sophie.tigerholm@gmail.com>
2024-05-29 11:01:24 +0000

    Add sendMail function

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,1 +1,14 @@
-console.log("Hello");
+function sendMail(contactForm) {
+    emailjs.send("service_76in8zc","rosie", {
+        "from_name": contactForm.name.value,
+        "project_request": contactForm.projectsummary.value,
+        "from_email": contactForm.emailaddress.value
+        })
+        .then(
+            function(response) {
+                console.log("SUCCESS", response);
+            },
+            function(error) {
+                console.log("FAILED", error);
+            });
+}
